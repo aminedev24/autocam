65fbdf18cc1ee788672d5058ceba6a84ffb2b00d
aminedev24 <54458000+aminedev24@users.noreply.github.com>
2023-05-14 23:46:42 +0200

    Update hero.js
    added process.env to image src

--- src/hero.js
+++ src/hero.js
@@ -12,14 +12,14 @@ function Hero() {
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="/img/BARGAINSALE-sale_n.jpg"
+            src={process.env.PUBLIC_URL + "/img/BARGAINSALE-sale_n.jpg"}
             alt="First slide"
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="/img/FLASHSALE-sale_n.jpg"
+            src={process.env.PUBLIC_URL + "/img/BARGAINSALE-sale_n.jpg"}
             alt="Second slide"
           />
         </Carousel.Item>
